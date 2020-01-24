0a315428dac71967a4ae5196d509b3864cb57e6c
Blair <abrgroup@gmail.com>
2020-01-23 19:56:13 -0500

    MVP complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,13 +20,14 @@ const UserForm = ({ errors, touched, values, status }) => {
         <p>
           <Field type="text" name="name" placeholder="Name" />
         </p>
-
+        {errors.name && <p className="error">{errors.name}</p>}
         <p>
           <Field type="text" name="email" placeholder="Email" />
         </p>
-
+        {errors.email && <p className="error">{errors.email}</p>}
         <p>
           <Field type="text" name="password" placeholder="Password" />
+          {errors.password && <p className="error">{errors.password}</p>}
         </p>
         <p>
           <label className="checkbox-container">
@@ -38,6 +39,9 @@ const UserForm = ({ errors, touched, values, status }) => {
             />
           </label>
         </p>
+        {errors.termsofservice && (
+          <p className="error">{errors.termsofservice}</p>
+        )}
         <p>
           <button type="submit">Submit!</button>
         </p>
@@ -47,13 +51,14 @@ const UserForm = ({ errors, touched, values, status }) => {
           <li>Name: {el.name}</li>
           <li>Email: {el.email}</li>
           <li>Password: {el.password}</li>
+          {/* <li>Agree to Terms: {el.termsofservice}</li> */}
         </ul>
       ))}
     </div>
   );
 };
 const FormikUserForm = withFormik({
-  mapPropsToValues({ users }) {
+  mapPropsToValues({ user }) {
     return {
       termsofservice: false,
       name: "",
@@ -61,13 +66,27 @@ const FormikUserForm = withFormik({
       password: ""
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .required("Name is required")
+      .max(50, "Please enter no more than 50 characters"),
+    email: Yup.string()
+      .email("Email address isn't valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .required("No password provided.")
+      .min(8, "Password is too short - should be 8 chars minimum."),
+    termsofservice: Yup.bool().oneOf([true], "Terms of service are required")
+  }),
+
   handleSubmit(values, { setStatus, resetForm }) {
     console.log("submitting! ", values);
     axios
       .post("https://reqres.in/api/users/", values)
       .then(res => {
         console.log("success", res);
-        setStatus(res.data); // get just the animal data from the REST api
+        setStatus(res.data); // get just the user data from the REST api
         resetForm();
       })
       .catch(err => console.log(err.response));
